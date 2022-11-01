477a9ed7e9547fd552f704f1f70d4aa2a10f763f
gabrielluizep <gabrielluizep@hotmail.com>
2022-11-01 08:35:53 -0300

    :sparkles: developed phones class

--- app/src/main/java/engtelecom/poo/Phones.java
+++ app/src/main/java/engtelecom/poo/Phones.java
@@ -4,22 +4,42 @@ import java.util.HashMap;
 
 public class Phones {
 
+  private static final String PHONE_REGEX = "^[0-9]*$";
+
   private HashMap<String, String> phones;
 
   Phones() {
     this.phones = new HashMap<String, String>();
   }
 
-  public boolean add(String label, String phone) {
-    return false;
+  public boolean add(String label, String email) {
+    if (this.phones.containsKey(label) || !email.matches(PHONE_REGEX)) {
+      return false;
+    }
+
+    this.phones.put(label, email);
+
+    return true;
   }
 
   public boolean remove(String label) {
-    return false;
+    if (!this.phones.containsKey(label)) {
+      return false;
+    }
+
+    this.phones.remove(label);
+
+    return true;
   }
 
   public boolean update(String label, String newPhone) {
-    return false;
+    if ((!this.phones.containsKey(label)) || (!newPhone.matches(PHONE_REGEX))) {
+      return false;
+    }
+
+    this.phones.put(label, newPhone);
+
+    return true;
   }
 
   // TODO implement
